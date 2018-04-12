ee024dcf1d8967fc29cc79d9b31e6cb3f58ac962
Francisco Henao Diaz <luisfranciscohenaodiaz@MacBook-Pro-de-Francisco.local>
2018-04-12 11:56:43 -0700

    Add: axis labs to fossil graph

--- R/graphs.R
+++ R/graphs.R
@@ -17,10 +17,11 @@ summary_paleo_results<-read.delim("output/summary_paleo_results.csv", sep = ",",
 long_paleo_results<-(gather(summary_paleo_results, key="Rate", value = "Estimate",mean.clade.mu:mean.clade.origination))
 long_paleo_results$Rate<-recode(long_paleo_results$Rate, `mean.clade.mu`="Extinction", `mean.clade.origination`="Origination")
 
+express2<-expression(paste("Ln estimate (genera ", Myr^-1,")"),sep=" ")
 p2<-ggplot(long_paleo_results,aes(x=log(Duration),y=log(Estimate),shape=Rate, group=Rate)) + 
   geom_point(aes(size=Ngen,colour=freqRat)) + theme_bw()
 
-p2+labs(x="Ln duration (Myr)", y="Estimate") + theme(axis.title = element_text(size=15)) + 
+p2+labs(x="Ln duration (Myr)", y=express2) + theme(axis.title = element_text(size=15)) + 
   geom_smooth(aes(linetype=Rate),method=lm, se=F,alpha=.1)+
   scale_size_continuous(name = "Clade\ngenera") + 
   scale_colour_gradientn(name="Record\ncompletedness",colours = rev(brewer.pal(4,"Spectral"))) + 
